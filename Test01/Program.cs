7e7b56282541c2d52a0c8e14c49ea6fecf6a8cc4
Alina_Borovskih <alinaberseneva@icloud.com>
2022-06-13 22:21:10 +0300

    Add test01

---
+++ Test01/Program.cs
@@ -0,0 +1,33 @@
+﻿void Print(string[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write(mass[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите размер массива: ");
+int N = int.Parse(Console.ReadLine());
+string[] mass = new string[N];
+Console.WriteLine("Введите строку: ");
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = Console.ReadLine();
+}
+
+Console.WriteLine();
+
+int count = 0;
+string[] mass2 = new string[N];
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i].Length <= 3)
+    {
+        mass2[count] = mass[i];
+        count++;
+    }
+}
+
+Print(mass2);
